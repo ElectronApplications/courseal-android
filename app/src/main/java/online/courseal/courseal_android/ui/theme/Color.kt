38538a47d49840e8731d817dab7c83746e1ba019
ElectronApps <barbwirek4@gmail.com>
2024-03-17 16:19:32 +0800

    Improved welcome screen

--- app/src/main/java/online/courseal/courseal_android/ui/theme/Color.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightTopGradient = Color(0xFFD4E3EB)
+val LightBottomGradient = Color(0xFFCFC69D)
+
+val DarkTopGradient = Color(0xFF33444D)
+val DarkBottomGradient = Color(0xFF78735B)
