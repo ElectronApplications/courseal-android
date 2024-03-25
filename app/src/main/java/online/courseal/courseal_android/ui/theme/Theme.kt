eb0cc2e20d653d549ccb2bff608fa37a53b5e81b
ElectronApps <klimchikeg@gmail.com>
2024-03-25 22:23:22 +0800

    Finished welcome screen

--- app/src/main/java/online/courseal/courseal_android/ui/theme/Theme.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/theme/Theme.kt
@@ -1,64 +1,59 @@
 package online.courseal.courseal_android.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF0066FF),
+    secondary = Color(0xFF121D2F),
+    tertiary = Color(0xFF3F3F3F),
+    background = Color(0xFF242424),
+    surface = Color(0xFF3A3A3A),
+
+    onPrimary = Color.White,
+    onSecondary = Color(0xFF4493F8),
+    onTertiary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = Color(0xFF0066FF),
+    secondary = Color(0xFFDDF4FF),
+    tertiary = Color(0xFFEEF0F7),
     background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    surface = Color(0xFFF2F2F2),
+
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
+    onSecondary = Color(0xFF0969DA),
+    onTertiary = Color(0xFF525F7F),
     onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF2D2D2D),
 )
 
 private val DarkCoursealPalette = CoursealPalette(
     welcomeGradientTop = DarkTopGradient,
-    welcomeGradientBottom = DarkBottomGradient
+    welcomeGradientBottom = DarkBottomGradient,
+    onWelcomeGradient = DarkGradientText
 )
 
 private val LightCoursealPalette = CoursealPalette(
     welcomeGradientTop = LightTopGradient,
-    welcomeGradientBottom = LightBottomGradient
+    welcomeGradientBottom = LightBottomGradient,
+    onWelcomeGradient = LightGradientText
 )
 
 @Composable
 fun CoursealTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val (colorScheme, coursealPalette) = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) Pair(dynamicDarkColorScheme(context), DarkCoursealPalette) else Pair(dynamicLightColorScheme(context), LightCoursealPalette)
-        }
-
         darkTheme -> Pair(DarkColorScheme, DarkCoursealPalette)
         else -> Pair(LightColorScheme, LightCoursealPalette)
     }
--- app/src/main/java/online/courseal/courseal_android/ui/theme/Type.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/theme/Type.kt
@@ -2,7 +2,6 @@ package online.courseal.courseal_android.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
