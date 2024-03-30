8e3e59c6bd597752a6e84b1824020fe4efa6d21c
ElectronApps <barbwirek4@gmail.com>
2024-03-30 10:29:12 +0800

    Implement a silly animation

--- app/src/main/java/online/courseal/courseal_android/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/screens/WelcomeScreen.kt
@@ -2,7 +2,10 @@ package online.courseal.courseal_android.ui.screens
 
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -24,6 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -99,14 +103,24 @@ fun CoursealGradient(
             color = LocalCoursealPalette.current.onWelcomeGradient
         )
 
-        Image(
+        val coursealVisibility = remember {
+            MutableTransitionState(false).apply {
+                targetState = true
+            }
+        }
+        AnimatedVisibility(
             modifier = Modifier
                 .width(150.dp)
                 .align(alignment = Alignment.CenterHorizontally),
-            contentScale = ContentScale.FillWidth,
-            painter = painterResource(id = R.drawable.courseal_not_rounded),
-            contentDescription = context.getString(R.string.logo)
-        )
+            visibleState = coursealVisibility,
+            enter = slideInVertically(initialOffsetY = { it }, animationSpec = tween(1000))
+        ) {
+            Image(
+                contentScale = ContentScale.FillWidth,
+                painter = painterResource(id = R.drawable.courseal_not_rounded),
+                contentDescription = context.getString(R.string.logo)
+            )
+        }
     }
 }
 
