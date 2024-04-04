757fed3aa1ed1dbf58ac1f2eb74e7a023af30adc
ElectronApps <barbwirek4@gmail.com>
2024-04-04 20:33:47 +0800

    Disable selection in the delimiter

--- app/src/main/java/online/courseal/courseal_android/ui/components/editorjs/content/EditorJSDelimiterComponent.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/components/editorjs/content/EditorJSDelimiterComponent.kt
@@ -1,5 +1,6 @@
 package online.courseal.courseal_android.ui.components.editorjs.content
 
+import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,10 +11,12 @@ import androidx.compose.ui.text.style.TextAlign
 fun EditorJSDelimiterComponent(
     modifier: Modifier = Modifier
 ) {
-    Text(
-        modifier = modifier,
-        textAlign = TextAlign.Center,
-        text = "* * *",
-        style = MaterialTheme.typography.headlineLarge
-    )
+    DisableSelection {
+        Text(
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            text = "* * *",
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
 }
