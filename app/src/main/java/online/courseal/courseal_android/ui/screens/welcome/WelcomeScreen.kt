accb808350c5518973fcde2d6c1d7aad233c8203
ElectronApps <barbwirek4@gmail.com>
2024-04-08 00:19:58 +0800

    Switch to TextOverflow.Ellipsis

--- app/src/main/java/online/courseal/courseal_android/ui/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/screens/welcome/WelcomeScreen.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -20,6 +22,7 @@ import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -34,6 +37,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
@@ -104,13 +108,12 @@ fun WelcomeScreen(
                         text = stringResource(R.string.connecting_to) + " "
                     )
 
-                    val displayUrl = if (welcomeUiState.serverUrl.length > 24) {
-                        welcomeUiState.serverUrl.take(21) + "..."
-                    } else welcomeUiState.serverUrl
-
                     Text(
-                        text = displayUrl,
-                        textDecoration = TextDecoration.Underline
+                        modifier = Modifier.weight(1f, fill = false),
+                        text = welcomeUiState.serverUrl,
+                        textDecoration = TextDecoration.Underline,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1
                     )
                     Icon(
                         modifier = Modifier
