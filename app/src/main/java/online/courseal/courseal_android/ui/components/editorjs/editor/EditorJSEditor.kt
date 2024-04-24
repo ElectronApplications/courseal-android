b2004051564c205860cc8e7eb5ac18d874a6f420
ElectronApps <barbwirek4@gmail.com>
2024-04-24 13:36:42 +0800

    Add image picker

--- app/src/main/java/online/courseal/courseal_android/ui/components/editorjs/editor/EditorJSEditor.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/components/editorjs/editor/EditorJSEditor.kt
@@ -1,12 +1,22 @@
 package online.courseal.courseal_android.ui.components.editorjs.editor
 
 import android.annotation.SuppressLint
+import android.net.Uri
 import android.webkit.JavascriptInterface
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -20,9 +30,15 @@ import online.courseal.courseal_android.data.coursedata.editorjs.EditorJSContent
 fun EditorJSEditorComponent(
     modifier: Modifier = Modifier,
     data: EditorJSContent,
-    onDataChange: (EditorJSContent) -> Unit
+    onDataChange: (EditorJSContent) -> Unit,
+    saveEndpoint: String
 ) {
     val isDark = isSystemInDarkTheme()
+
+    var pickResult by remember { mutableStateOf<Uri?>(null) }
+    val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+        pickResult = it
+    }
 
     AndroidView(
         modifier = modifier
@@ -35,6 +51,23 @@ fun EditorJSEditorComponent(
                 settings.loadWithOverviewMode = true
                 settings.useWideViewPort = true
                 settings.setSupportZoom(false)
+                settings.allowFileAccess = true
+                settings.allowContentAccess = true
+
+                webChromeClient = object: WebChromeClient() {
+                    override fun onShowFileChooser(
+                        webView: WebView?,
+                        filePathCallback: ValueCallback<Array<Uri>>?,
+                        fileChooserParams: FileChooserParams?
+                    ): Boolean {
+                        pickMedia.launch(PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly))
+                        if (pickResult != null) {
+                            filePathCallback?.onReceiveValue(arrayOf(pickResult!!))
+                            return true
+                        }
+                        return false
+                    }
+                }
 
                 addJavascriptInterface(object {
                     @JavascriptInterface
@@ -45,6 +78,11 @@ fun EditorJSEditorComponent(
                     @JavascriptInterface
                     fun setData(data: String) {
                         onDataChange(Json.decodeFromString(data))
+                    }
+
+                    @JavascriptInterface
+                    fun getSaveEndpoint(): String {
+                        return saveEndpoint
                     }
 
                     @JavascriptInterface
