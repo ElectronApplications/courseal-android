25f8b6bda0d8704917560db3dc3bbff40449f7c8
ElectronApps <klimchikeg@gmail.com>
2024-05-05 12:34:01 +0800

    Fix a crash caused by uploading images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "online.courseal.courseal_android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.1"
+        versionCode = 5
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
