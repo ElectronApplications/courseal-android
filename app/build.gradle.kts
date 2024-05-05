55501312742002682e80d95aa3ea2c800602619e
ElectronApps <klimchikeg@gmail.com>
2024-05-05 17:02:38 +0800

    Increase version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "online.courseal.courseal_android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.2"
+        versionCode = 6
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
