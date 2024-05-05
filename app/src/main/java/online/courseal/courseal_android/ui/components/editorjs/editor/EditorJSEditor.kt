e56d618b67ecb6fbc3f3f3ec9df2c2fb18b691f8
ElectronApps <barbwirek4@gmail.com>
2024-05-05 12:34:01 +0800

    Fix a crash caused by uploading images

--- app/src/main/java/online/courseal/courseal_android/ui/components/editorjs/editor/EditorJSEditor.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/components/editorjs/editor/EditorJSEditor.kt
@@ -15,7 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,7 +35,7 @@ fun EditorJSEditorComponent(
 ) {
     val isDark = isSystemInDarkTheme()
 
-    var pickCallback by rememberSaveable { mutableStateOf<ValueCallback<Array<Uri>>?>(null) }
+    var pickCallback by remember { mutableStateOf<ValueCallback<Array<Uri>>?>(null) }
     val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
         pickCallback?.onReceiveValue(it?.let { result -> arrayOf(result) })
     }
