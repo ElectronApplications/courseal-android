e8a12320fc3c65152bcb5c8d1deebe02109b1561
ElectronApps <klimchikeg@gmail.com>
2024-05-16 00:37:19 +0800

    Rewrite the code for doing lessons

--- app/src/main/java/online/courseal/courseal_android/ui/screens/course/LessonStartScreen.kt
+++ app/src/main/java/online/courseal/courseal_android/ui/screens/course/LessonStartScreen.kt
@@ -1,7 +1,13 @@
 package online.courseal.courseal_android.ui.screens.course
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -14,42 +20,58 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
 import online.courseal.courseal_android.R
+import online.courseal.courseal_android.data.coursedata.enrolltaskscomplete.TaskSingleExamAnswer
 import online.courseal.courseal_android.data.coursedata.examtasks.CoursealExamTaskMultiple
 import online.courseal.courseal_android.data.coursedata.examtasks.CoursealExamTaskSingle
+import online.courseal.courseal_android.data.coursedata.tasks.CoursealTaskMultiple
+import online.courseal.courseal_android.data.coursedata.tasks.CoursealTaskSingle
 import online.courseal.courseal_android.ui.OnUnrecoverable
+import online.courseal.courseal_android.ui.components.CoursealDropdownScreen
+import online.courseal.courseal_android.ui.components.CoursealErrorButton
 import online.courseal.courseal_android.ui.components.CoursealPrimaryButton
 import online.courseal.courseal_android.ui.components.CoursealSelectableButton
+import online.courseal.courseal_android.ui.components.CoursealSuccessButton
 import online.courseal.courseal_android.ui.components.ErrorDialog
 import online.courseal.courseal_android.ui.components.TopCancel
 import online.courseal.courseal_android.ui.components.adaptiveContainerWidth
 import online.courseal.courseal_android.ui.components.editorjs.content.EditorJSContentComponent
+import online.courseal.courseal_android.ui.theme.LocalCoursealPalette
+import online.courseal.courseal_android.ui.viewmodels.course.CourseViewModel
 import online.courseal.courseal_android.ui.viewmodels.course.LessonStartUiContent
 import online.courseal.courseal_android.ui.viewmodels.course.LessonStartUiError
 import online.courseal.courseal_android.ui.viewmodels.course.LessonStartViewModel
+import online.courseal.courseal_android.ui.viewmodels.course.LessonTask
+import online.courseal.courseal_android.ui.viewmodels.course.TaskAnswer
 
 @Composable
 fun LessonStartScreen(
     modifier: Modifier = Modifier,
     onGoBack: () -> Unit,
     onUnrecoverable: OnUnrecoverable,
-    lessonStartViewModel: LessonStartViewModel = hiltViewModel()
+    lessonStartViewModel: LessonStartViewModel = hiltViewModel(),
+    courseViewModel: CourseViewModel
 ) {
     val coroutineScope = rememberCoroutineScope()
     val lessonStartUiState by lessonStartViewModel.uiState.collectAsState()
@@ -79,7 +101,7 @@ fun LessonStartScreen(
             onDismissRequest = { warningShown = false },
             dismissButton = {
                 TextButton(onClick = { warningShown = false }) {
-                    Text("Dismiss")
+                    Text(stringResource(R.string.dismiss))
                 }
             },
             confirmButton = {
@@ -95,96 +117,162 @@ fun LessonStartScreen(
         )
     }
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-    ) {
-        Spacer(modifier = Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
-        TopCancel(onClick = onGoBack)
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
+            TopCancel(onClick = onGoBack)
 
-        if (lessonStartUiState.loading) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                CircularProgressIndicator()
-            }
-        } else {
-            Column(
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .adaptiveContainerWidth(800.dp)
-                    .padding(top = 12.dp)
-            ) {
-                when (val content = lessonStartUiState.currentContent) {
-                    is LessonStartUiContent.LectureContent -> {
-                        EditorJSContentComponent(
+            if (lessonStartUiState.loading) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator()
+                }
+            } else {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .adaptiveContainerWidth(800.dp)
+                        .padding(top = 12.dp)
+                ) {
+                    when (val content = lessonStartUiState.currentContent) {
+                        is LessonStartUiContent.LectureContent -> LessonLectureScreen(
                             modifier = Modifier
-                                .align(Alignment.CenterHorizontally)
-                                .fillMaxWidth(0.85f),
-                            content = content.content
+                                .align(Alignment.CenterHorizontally),
+                            content = content.content,
+                            lessonStartViewModel = lessonStartViewModel
                         )
-                    }
-                    is LessonStartUiContent.TaskContent -> {
-                        EditorJSContentComponent(
-                            modifier = Modifier
-                                .align(Alignment.CenterHorizontally)
-                                .fillMaxWidth(0.85f),
-                            content = when (content.content) {
-                                is CoursealExamTaskMultiple -> content.content.body
-                                is CoursealExamTaskSingle -> content.content.body
+                        is LessonStartUiContent.TaskContent -> {
+                            when (content.task) {
+                                is LessonTask.PracticeTrainingTask -> {
+                                    when (content.task.content) {
+                                        is CoursealTaskSingle -> LessonTaskSingleScreen(
+                                            modifier = Modifier
+                                                .align(Alignment.CenterHorizontally),
+                                            content = TaskSingleAnswer.PracticeTrainingTask(content.task.content),
+                                            lessonStartViewModel = lessonStartViewModel
+                                        )
+                                        is CoursealTaskMultiple -> LessonTaskMultipleScreen(
+                                            modifier = Modifier
+                                                .align(Alignment.CenterHorizontally),
+                                            content = TaskMultipleAnswer.PracticeTrainingTask(content.task.content),
+                                            lessonStartViewModel = lessonStartViewModel
+                                        )
+                                    }
+                                }
+                                is LessonTask.ExamTask -> {
+                                    when (content.task.content) {
+                                        is CoursealExamTaskSingle -> LessonTaskSingleScreen(
+                                            modifier = Modifier
+                                                .align(Alignment.CenterHorizontally),
+                                            content = TaskSingleAnswer.ExamTask(content.task.content),
+                                            lessonStartViewModel = lessonStartViewModel
+                                        )
+                                        is CoursealExamTaskMultiple -> LessonTaskMultipleScreen(
+                                            modifier = Modifier
+                                                .align(Alignment.CenterHorizontally),
+                                            content = TaskMultipleAnswer.ExamTask(content.task.content),
+                                            lessonStartViewModel = lessonStartViewModel
+                                        )
+                                    }
+                                }
                             }
-                        )
-
-                        Text(
-                            modifier = Modifier
-                                .align(Alignment.CenterHorizontally)
-                                .fillMaxWidth(0.85f),
-                            fontWeight = FontWeight.SemiBold,
-                            text = when (content.content) {
-                                is CoursealExamTaskSingle -> "Choose the correct answer"
-                                is CoursealExamTaskMultiple -> "Choose all correct answers"
-                            }
-                        )
-
-                        val options = when(content.content) {
-                            is CoursealExamTaskSingle -> content.content.options
-                            is CoursealExamTaskMultiple -> content.content.options.map { it.text }
                         }
-
-                        options.forEachIndexed { index, option ->
-                            CoursealSelectableButton(
+                        is LessonStartUiContent.ResultsContent -> {
+                            Text(
                                 modifier = Modifier
-                                    .padding(top = 6.dp)
                                     .align(Alignment.CenterHorizontally)
                                     .fillMaxWidth(0.85f),
-                                text = option,
-                                selected = index in lessonStartUiState.selectedOptions,
+                                text = if (content.completed) stringResource(R.string.lesson_passed)
+                                    else stringResource(R.string.lesson_not_passed),
+                                textAlign = TextAlign.Center,
+                                style = MaterialTheme.typography.headlineMedium
+                            )
+
+                            CoursealPrimaryButton(
+                                modifier = Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .fillMaxWidth(0.85f)
+                                    .padding(top = 12.dp),
+                                text = stringResource(R.string.confirm),
                                 onClick = {
-                                    lessonStartViewModel.selectOption(index)
+                                    courseViewModel.setNeedUpdate()
+                                    onGoBack()
                                 }
                             )
                         }
+                        null -> {}
                     }
-                    is LessonStartUiContent.ResultsContent -> {}
-                    null -> {}
                 }
+            }
+        }
 
-                CoursealPrimaryButton(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .fillMaxWidth(0.85f)
-                        .padding(top = 12.dp),
-                    text = stringResource(R.string.confirm),
-                    enabled = lessonStartUiState.confirmEnabled,
-                    onClick = {
-                        coroutineScope.launch {
-                            lessonStartViewModel.confirmAnswer()
-                        }
+        AnimatedVisibility(
+            visible = lessonStartUiState.taskCorrect != null,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
+                    ) {},
+                color = Color.Black.copy(alpha = 0.5f),
+                content = {}
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Spacer(modifier = Modifier.weight(1f, false))
+            AnimatedVisibility(
+                visible = lessonStartUiState.taskCorrect != null,
+            ) {
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = if (lessonStartUiState.taskCorrect == true) LocalCoursealPalette.current.successContainer
+                    else LocalCoursealPalette.current.errorContainer,
+                    contentColor = if (lessonStartUiState.taskCorrect == true) LocalCoursealPalette.current.onSuccess
+                    else LocalCoursealPalette.current.onError
+                ) {
+                    if (lessonStartUiState.taskCorrect == true) {
+                        CoursealSuccessButton(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .fillMaxWidth(0.85f)
+                                .padding(12.dp),
+                            text = stringResource(R.string.correct),
+                            onClick = {
+                                coroutineScope.launch {
+                                    lessonStartViewModel.hideTaskCorrect()
+                                }
+                            }
+                        )
+                    } else if (lessonStartUiState.taskCorrect == false) {
+                        CoursealErrorButton(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .fillMaxWidth(0.85f)
+                                .padding(24.dp),
+                            text = stringResource(R.string.wrong_answer),
+                            onClick = {
+                                coroutineScope.launch {
+                                    lessonStartViewModel.hideTaskCorrect()
+                                }
+                            }
+                        )
                     }
-                )
+                }
             }
         }
     }
---
+++ app/src/main/java/online/courseal/courseal_android/ui/screens/course/LessonTaskSingleScreen.kt
@@ -0,0 +1,107 @@
+package online.courseal.courseal_android.ui.screens.course
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import online.courseal.courseal_android.R
+import online.courseal.courseal_android.data.coursedata.enrolltaskscomplete.TaskSingleExamAnswer
+import online.courseal.courseal_android.data.coursedata.examtasks.CoursealExamTaskSingle
+import online.courseal.courseal_android.data.coursedata.tasks.CoursealTaskSingle
+import online.courseal.courseal_android.ui.components.CoursealPrimaryButton
+import online.courseal.courseal_android.ui.components.CoursealSelectableButton
+import online.courseal.courseal_android.ui.components.editorjs.content.EditorJSContentComponent
+import online.courseal.courseal_android.ui.viewmodels.course.LessonStartViewModel
+import online.courseal.courseal_android.ui.viewmodels.course.TaskAnswer
+
+sealed class TaskSingleAnswer {
+    class PracticeTrainingTask(val task: CoursealTaskSingle) : TaskSingleAnswer()
+    class ExamTask(val task: CoursealExamTaskSingle) : TaskSingleAnswer()
+}
+
+@Composable
+fun LessonTaskSingleScreen(
+    modifier: Modifier = Modifier,
+    content: TaskSingleAnswer,
+    lessonStartViewModel: LessonStartViewModel
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = modifier
+    ) {
+        EditorJSContentComponent(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(0.85f),
+            content = when (content) {
+                is TaskSingleAnswer.ExamTask -> content.task.body
+                is TaskSingleAnswer.PracticeTrainingTask -> content.task.body
+            }
+        )
+
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(0.85f),
+            fontWeight = FontWeight.SemiBold,
+            text = stringResource(R.string.choose_correct_answer)
+        )
+
+        val options = when(content) {
+            is TaskSingleAnswer.PracticeTrainingTask -> content.task.options
+            is TaskSingleAnswer.ExamTask -> content.task.options
+        }
+
+        var currentOption by rememberSaveable(content) { mutableStateOf<Int?>(null) }
+
+        options.forEachIndexed { index, option ->
+            CoursealSelectableButton(
+                modifier = Modifier
+                    .padding(top = 6.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.85f),
+                text = option,
+                selected = index == currentOption,
+                onClick = {
+                    currentOption = if (currentOption == index) {
+                        null
+                    } else {
+                        index
+                    }
+                }
+            )
+        }
+
+        CoursealPrimaryButton(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(0.85f)
+                .padding(top = 12.dp),
+            text = stringResource(R.string.confirm),
+            enabled = currentOption != null,
+            onClick = {
+                coroutineScope.launch {
+                    val answer = when (content) {
+                        is TaskSingleAnswer.PracticeTrainingTask -> TaskAnswer.PracticeTrainingAnswer(currentOption == content.task.correctOption)
+                        is TaskSingleAnswer.ExamTask -> TaskAnswer.ExamAnswer(TaskSingleExamAnswer(currentOption!!))
+                    }
+                    lessonStartViewModel.saveAnswer(answer)
+                }
+            }
+        )
+    }
+}
